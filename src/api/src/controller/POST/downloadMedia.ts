7f14f723ff5bf7a2ea25143fe39ecb826531920c
Miouss <samir.ghabi@gmail.com>
2022-12-07 18:34:55 +0100

    Fix way to handle webcontents

--- src/api/src/controller/POST/downloadMedia.ts
+++ src/api/src/controller/POST/downloadMedia.ts
@@ -6,40 +6,41 @@ import { readFile } from "jsonfile";
 
 import { Request, Response } from "express";
 
-import mainWindow from "../../../../index";
+import getWindow from "../../../../index";
 
 export default async function streamDownload(req: Request, res: Response) {
   console.time("Operations Completed in ");
 
   const { outputPath } = await readFile("./src/api/config.json");
 
+  const windowWebContents = getWindow().webContents;
   try {
-    mainWindow.webContents.send("download-fully-starts");
+    windowWebContents.send("download-fully-starts");
 
-    mainWindow.webContents.send("recovering-frags-playlists-starts");
+    windowWebContents.send("recovering-frags-playlists-starts");
     
     const videoUrlList = await downloadVodPlaylist(req.body.videoUrl);
     const audioUrlList = await downloadVodPlaylist(req.body.audioUrl);
 
-    mainWindow.webContents.send("recovering-frags-playlists-ends");
+    windowWebContents.send("recovering-frags-playlists-ends");
 
-    mainWindow.webContents.send("downloading-frags-starts");
+    windowWebContents.send("downloading-frags-starts");
 
     await downloadVodFragments(videoUrlList, "ts", outputPath);
     await downloadVodFragments(audioUrlList, "aac", outputPath);
 
-    mainWindow.webContents.send("downloading-frags-ends");
+    windowWebContents.send("downloading-frags-ends");
 
-    mainWindow.webContents.send("merging-starts");
+    windowWebContents.send("merging-starts");
 
     await createMergeFile("listVideo", videoUrlList, outputPath, "ts");
     await createMergeFile("listAudio", audioUrlList, outputPath, "aac");
 
     await handleMerging(req.body.vodTitle, outputPath);
 
-    mainWindow.webContents.send("merging-ends");
+    windowWebContents.send("merging-ends");
 
-    mainWindow.webContents.send("download-fully-completed");
+    windowWebContents.send("download-fully-completed");
 
     console.timeEnd("Operations Completed in ");
 
@@ -47,6 +48,7 @@ export default async function streamDownload(req: Request, res: Response) {
     res.json({ "Download": true });
   } catch (error) {
     res.status(404);
+    console.log(error);
     res.json({ errorMessage: error });
   }
 }
