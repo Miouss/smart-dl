467bd7be8991aff3458efe685ef2eaa819843ed4
Miouss <samir.ghabi@gmail.com>
2022-12-14 17:32:18 +0100

    Render tasks of the current step

---
+++ src/React/components/MediaMenu/Card/Stepper/StepperSteps.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import StepsTask from "./Steps/StepsTask";
+
+import { Task } from "../../../../../types/Task";
+
+interface Props {
+  activeStep: number;
+  videoFragsDlTask: Task;
+  audioFragsDlTask: Task;
+  mergingVideoPartTask: Task;
+  mergingAudioPartTask: Task;
+  mergingPartsTask: Task;
+}
+
+export default function StepperSteps({
+  activeStep,
+  videoFragsDlTask,
+  audioFragsDlTask,
+  mergingVideoPartTask,
+  mergingAudioPartTask,
+  mergingPartsTask,
+}: Props) {
+  switch (activeStep) {
+    case 0:
+      return (
+        <>
+          <StepsTask task={videoFragsDlTask} />
+          <StepsTask task={audioFragsDlTask} />
+        </>
+      );
+    case 1:
+      return (
+        <>
+          <StepsTask task={mergingVideoPartTask} />
+          <StepsTask task={mergingAudioPartTask} />
+        </>
+      );
+    case 2:
+      return <StepsTask task={mergingPartsTask} />;
+    default:
+      return null;
+  }
+}
