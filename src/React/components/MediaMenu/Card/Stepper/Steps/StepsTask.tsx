cc0d291b3810e490932e2903a2dd3853b1ec397b
Miouss <samir.ghabi@gmail.com>
2022-12-14 17:30:22 +0100

    Render current task with styled font

---
+++ src/React/components/MediaMenu/Card/Stepper/Steps/StepsTask.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { TaskLabel } from "../../../../styled/TaskLabel";
+import TaskState from "./Task/TaskState";
+
+import { Task } from "../../../../../../types/Task";
+
+interface Props {
+  task: Task;
+}
+
+export default function StepsTasks({ task }: Props) {
+  return (
+    <TaskLabel
+      component="span"
+      variant="h6"
+      started={task.started ? 1 : 0}
+      done={task.done ? 1 : 0}
+    >
+      <TaskState task={task} />
+    </TaskLabel>
+  );
+}
