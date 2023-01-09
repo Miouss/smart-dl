bfb74b9da788d5a7ed660923a0d9eb5aaee7cbfd
Miouss <samir.ghabi@gmail.com>
2023-01-09 23:27:48 +0100

    Isolated tasks of a list

---
+++ src/api/src/tasks/tasks.ts
@@ -0,0 +1,28 @@
+import getWindow from "../../../index";
+import merging from "../utilFcts/Merging";
+
+export const mergingTask = (taskTitle: string, mergingInstruction: string[], startEvent: string, endEvent:string) => {
+  const windowWebContents = getWindow().webContents;
+
+  return {
+    title: taskTitle,
+    task: async () => {
+      windowWebContents.send(startEvent);
+      await merging("ffmpeg", mergingInstruction);
+      windowWebContents.send(endEvent);
+    },
+  };
+};
+
+export const deletingTask = (taskTitle: string, mergingFile: string, outputPath: string, startEvent: string, endEvent:string) => {
+  const windowWebContents = getWindow().webContents;
+
+  return {
+    title: taskTitle,
+    task: async () => {
+      windowWebContents.send(startEvent);
+      await merging(`${mergingFile}.bat`, [`${outputPath}`]);
+      windowWebContents.send(endEvent);
+    },
+  }
+}
