4a4919dfdeff26d530f2a4c68b29db9433352095
Miouss <samir.ghabi@gmail.com>
2023-01-10 17:42:13 +0100

    Updated to handle new way to call steppers

--- src/React/components/MenuCard/CardStepper.tsx
+++ src/React/components/MenuCard/CardStepper.tsx
@@ -1,230 +1,42 @@
 import React, { useState } from "react";
 
-import { styled } from "@mui/material/styles";
-import { Box, Stack } from "@mui/material";
+import { Box } from "@mui/material";
 
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
-
-import FileCopyIcon from "@mui/icons-material/FileCopy";
-import DifferenceIcon from "@mui/icons-material/Difference";
-import MovieIcon from "@mui/icons-material/Movie";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
-
-import StepperSteps from "./Stepper/StepperSteps";
-import { StepIconProps } from "@mui/material/StepIcon";
-import StepConnector, {
-  stepConnectorClasses,
-} from "@mui/material/StepConnector";
-
-import { task } from "../../utils/task";
-
-import { Task } from "../../../types/Task";
-
-type Media = "Video" | "Audio";
+import StepperDownloadSteps from "./Stepper/StepperDownloadSteps";
+import StepperCancelSteps from "./Stepper/StepperCancelSteps";
 
 export default function MediaMenuStepper() {
   const [activeStep, setActiveStep] = useState<number>(0);
-
-  const steps = [
-    "Downloading Fragments",
-    "Merging Fragments into Video and Audio Parts",
-    "Merging Audio with Video",
-    "Done",
-  ];
+  const [isCanceled, setIsCanceled] = useState(false);
 
   window.downloadAPI.onDownloadingFragsEnds(() => setActiveStep(1));
   window.downloadAPI.onMergingPartsEnds(() => setActiveStep(2));
   window.downloadAPI.onMergingEnds(() => setActiveStep(4));
 
-  const [audioFrags, setAudioFrags] = useState<Task>({
-    title: "waiting to download audio fragments",
-    started: false,
-    done: false,
-  });
-  const [videoFrags, setVideoFrags] = useState<Task>({
-    title: "waiting to download video fragments",
-    started: false,
-    done: false,
-  });
-
-  const [videoPart, setVideoPart] = useState<Task>({
-    title: "waiting to merge video fragments into single part",
-    started: false,
-    done: false,
-  });
-  const [audioPart, setAudioPart] = useState<Task>({
-    title: "waiting to merge audio fragments into single part",
-    started: false,
-    done: false,
-  });
-
-  const [parts, setParts] = useState<Task>({
-    title: "waiting to merge video and audio parts into mp4 file",
-    started: false,
-    done: false,
-  });
-
-  const download = {
-    audioFrags,
-    videoFrags,
-  };
-  const merge = {
-    videoPart,
-    audioPart,
-    parts,
-  };
-
-  window.downloadAPI.onMergingVideoStarts(() =>
-    task.start(setVideoPart, "Merging video's fragments into single part")
-  );
-  window.downloadAPI.onMergingAudioStarts(() =>
-    task.start(setAudioPart, "Merging audio's fragments into single part")
-  );
-
-  window.downloadAPI.onMergingVideoEnds(() =>
-    task.end(
-      setVideoPart,
-      "Video's fragments had been merged into single part successfully"
-    )
-  );
-  window.downloadAPI.onMergingAudioEnds(() =>
-    task.end(
-      setAudioPart,
-      "Audio's fragments had been merged into single part successfully"
-    )
-  );
-
-  window.downloadAPI.onMergingPartsStarts(() =>
-    task.start(setParts, "Merging video and audio parts into MP4 file")
-  );
-  window.downloadAPI.onMergingPartsEnds(() =>
-    task.end(setParts, "Video and audio parts merged sucessfully")
-  );
-
-  window.downloadAPI.onUpdateDownloadSteps(
-    (_: unknown, taskTitle: string, mediaType: Media) => {
-      mediaType === "Audio"
-        ? task.update(setAudioFrags, taskTitle)
-        : task.update(setVideoFrags, taskTitle);
+  window.downloadAPI.onDeletingFragsEnds(() => {
+    if(isCanceled){
+      setActiveStep(2)
     }
-  );
-
-  window.downloadAPI.onDownloadStepsEnds((_: unknown, mediaType: Media) => {
-    mediaType === "Audio"
-      ? task.end(
-          setAudioFrags,
-          "Audio's fragments had been downloaded successfully"
-        )
-      : task.end(
-          setVideoFrags,
-          "Video's fragments had been downloaded successfully"
-        );
   });
 
-  const CustomizedStepConnector = styled(StepConnector)(({ theme }) => ({
-    [`&.${stepConnectorClasses.alternativeLabel}`]: {
-      top: 22,
-    },
-    [`&.${stepConnectorClasses.active}`]: {
-      [`& .${stepConnectorClasses.line}`]: {
-        backgroundColor: "rgba(255, 162, 0, 1)",
-      },
-    },
-    [`&.${stepConnectorClasses.completed}`]: {
-      [`& .${stepConnectorClasses.line}`]: {
-        backgroundColor: "#2e7d32",
-      },
-    },
-    [`& .${stepConnectorClasses.line}`]: {
-      height: 3,
-      border: 0,
-      backgroundColor:
-        theme.palette.mode === "dark" ? theme.palette.grey[800] : "#eaeaf0",
-      borderRadius: 1,
-    },
-  }));
+  window.downloadAPI.onDeletingPartsEnds(() => {
+    if(isCanceled){
+      setActiveStep(4)
+    }
+  });
 
-  const CustomizedStepIconContainer = styled("div")<{
-    ownerState: { completed?: boolean; active?: boolean };
-  }>(({ ownerState }) => ({
-    zIndex: 1,
-    width: 50,
-    height: 50,
-    display: "flex",
-    borderRadius: "50%",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(153, 153, 153, 1)",
-    ...(ownerState.active && {
-      backgroundColor: "rgba(255, 162, 0, 1)",
-    }),
-    ...(ownerState.completed && {
-      backgroundColor: "#2e7d32",
-    }),
-  }));
-
-  function CustomizedStepIcon(props: StepIconProps) {
-    const { active, completed, className } = props;
-
-    const icons: { [index: string]: React.ReactElement } = {
-      1: <FileCopyIcon />,
-      2: <DifferenceIcon />,
-      3: <MovieIcon />,
-      4: <ThumbUpIcon />,
-    };
-
-    return (
-      <CustomizedStepIconContainer
-        ownerState={{ completed, active }}
-        className={className}
-      >
-        {icons[String(props.icon)]}
-      </CustomizedStepIconContainer>
-    );
-  }
+  window.downloadAPI.onCancelStarts(() => {
+    setIsCanceled(true)
+    setActiveStep(1);
+  });
 
   return (
     <Box sx={{ width: "100%" }}>
-      <Stepper
-        activeStep={activeStep}
-        connector={<CustomizedStepConnector />}
-        alternativeLabel
-      >
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel
-              StepIconComponent={CustomizedStepIcon}
-              sx={{
-                "& .MuiStepLabel-alternativeLabel": {
-                  fontFamily: "Roboto Slab",
-                  fontStyle: "normal",
-                  fontWeight: "400",
-                  fontSize: "14px",
-                  lineHeight: "18px",
-                },
-
-                "& .Mui-active.MuiStepLabel-alternativeLabel": {
-                  color: "#F2F2F2",
-                },
-                "& .Mui-completed.MuiStepLabel-alternativeLabel": {
-                  color: "white",
-                },
-              }}
-            >
-              {label}
-            </StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-
-      <Stack direction={"column"} marginTop={"1rem"} marginBottom={"1rem"}>
-        <StepperSteps
-          activeStep={activeStep}
-          tasks={{ ...download, ...merge }}
-        />
-      </Stack>
+      {isCanceled ? (
+        <StepperCancelSteps activeStep={activeStep} />
+      ) : (
+        <StepperDownloadSteps activeStep={activeStep} />
+      )}
     </Box>
   );
 }
