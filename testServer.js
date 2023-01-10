5ae6556390f5ab145e63cda38d533b9bbba5aaed
Miouss <samir.ghabi@gmail.com>
2023-01-10 19:23:26 +0100

    removed useless files

--- testServer.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const express = require("express");
-
-const appExpress = express();
-
-appExpress.listen(8000, () => {
-    console.log("server is running...");
-});
