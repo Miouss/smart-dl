f316a248fc622f1d59c951aea57524d6a6b17836
Miouss <samir.ghabi@gmail.com>
2023-01-14 21:25:36 +0100

    Refactored for better scalability/readability

--- src/React/utils/alertMsg.tsx
+++ src/React/utils/alertMsg.tsx
@@ -1,17 +1,38 @@
 import { AlertColor } from "@mui/material";
 
 export function alertMsgOutputPath(
-  outputPath: undefined |string,
+  outputPath: undefined | string,
   triggerState: undefined | boolean
 ) {
-    
-  return outputPath ? {
-    severity: "info" as AlertColor,
-    message: `Your save location is : '${outputPath}'`,
+  return outputPath
+    ? infoAlert(`Your save location is : '${outputPath}'`, triggerState)
+    : warningAlert("There is no save location", triggerState);
+}
+
+export function alertMsgAccount(
+  username: undefined | string,
+  triggerState: undefined | boolean
+) {
+  return username
+    ? infoAlert(`The account named ${username} will be use`, triggerState)
+    : warningAlert(`There is no account saved`, triggerState);
+}
+
+
+/* Utils */
+
+function createAlert(severity: AlertColor, message: string, triggerState: undefined | boolean) {
+  return {
+    severity: severity,
+    message: message,
     trigger: !triggerState ?? true,
-  } : {
-    severity: "warning" as AlertColor,
-    message: `There is no save location`,
-    trigger: !triggerState ?? true,  
   };
+}
+
+function infoAlert(message: string, triggerState: undefined | boolean) {
+  return  createAlert("info", message, triggerState);
+}
+
+function warningAlert(message: string, triggerState: undefined | boolean) {
+  return createAlert("warning", message, triggerState);
 }
