e198726225f651ab42ede81a38124ab463e1890f
Miouss <samir.ghabi@gmail.com>
2023-01-14 21:24:33 +0100

    Refactored to handle credentials related events

--- src/React/components/Form/CredentialsLabelSwitch.tsx
+++ src/React/components/Form/CredentialsLabelSwitch.tsx
@@ -1,26 +1,89 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { FormControlLabel, Switch } from "@mui/material";
-
+import { AlertMsg } from "../../../types/AlertMsg";
+import { alertMsgAccount } from "../../utils/alertMsg";
 
 interface Props {
-  control: Dispatch<SetStateAction<boolean>>;
+  setCheckState: Dispatch<SetStateAction<boolean>>;
   id: string;
   label: string;
-  checked?: boolean
+  checked: boolean;
 }
 
-export default function CredentialsLabelSwitch({control, id, label, checked} : Props){
-    return <FormControlLabel
-    id={id}
-    control={
-      <Switch
-        color="primary"
-        onChange={() =>
-          control((state) => !state)
-        }
-        checked={checked}
-      />
-    }
-    label={label}
-  />
+interface GeneralProps extends Props {
+  eventEmitter?: () => void;
+}
+
+interface SpecificProps extends Props {
+  setAlertMsg: Dispatch<SetStateAction<AlertMsg>>;
+}
+
+export function UseSavedCredentialsSwitch({
+  setAlertMsg,
+  setCheckState,
+  id,
+  label,
+  checked
+}: SpecificProps) {
+  const eventEmitter = () => window.fileSystemAPI.retrieveAccount();
+
+  useEffect(() => {
+    window.fileSystemAPI.onAccountRetrieved((_: unknown, username?: string) => {
+      setAlertMsg((state) => alertMsgAccount(username, state?.trigger));
+      username === ""
+        ? setCheckState(false)
+        : setCheckState(true);
+    });
+  }, []);
+
+  return (
+    <CredentialsLabelSwitch
+      id={id}
+      label={label}
+      checked={checked}
+      eventEmitter={eventEmitter}
+      setCheckState={setCheckState}
+    />
+  );
+}
+
+export function SavedCredentialsSwitch({ setCheckState, id, label, checked }: SpecificProps) {
+
+  return (
+    <CredentialsLabelSwitch
+      id={id}
+      label={label}
+      checked={checked}
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      eventEmitter={() => {}}
+      setCheckState={setCheckState}
+    />
+  );
+}
+
+function CredentialsLabelSwitch({
+  id,
+  label,
+  checked,
+  eventEmitter,
+  setCheckState,
+}: GeneralProps) {
+  return (
+    <FormControlLabel
+      id={id}
+      control={
+        <Switch
+          color="primary"
+          onChange={() => {
+            setCheckState((state) => {
+              state ? null : eventEmitter();
+              return !state;
+            });
+          }}
+          checked={checked}
+        />
+      }
+      label={label}
+    />
+  );
 }
