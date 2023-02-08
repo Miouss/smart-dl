a462352e82410114f44c10dad91796f2db478f22
Miouss <samir.ghabi@gmail.com>
2023-02-08 21:42:37 +0100

    fix merging process for MAC OS

--- src/Node/models/POST/DownloadMedia/utils/tasks/merging.ts
+++ src/Node/models/POST/DownloadMedia/utils/tasks/merging.ts
@@ -17,7 +17,7 @@ export async function mergeVideo(saveLocation: string) {
     "listVideo.txt",
     "-c",
     "copy",
-    `${saveLocation}\\output.ts`,
+    `"${saveLocation}/output.ts"`,
   ];
 
   fireEvent("merging-video-starts");
@@ -36,7 +36,7 @@ export async function mergeAudio(saveLocation: string) {
     "listAudio.txt",
     "-c",
     "copy",
-    `${saveLocation}\\output.aac`,
+    `"${saveLocation}/output.aac"`,
   ];
 
   fireEvent("merging-audio-starts");
@@ -48,12 +48,12 @@ export async function mergeParts(saveLocation: string, vodTitle: string) {
   const instruction = [
     "-y",
     "-i",
-    `${saveLocation}\\output.ts`,
+    `${saveLocation}/output.ts`,
     "-i",
-    `${saveLocation}\\output.aac`,
+    `${saveLocation}/output.aac`,
     "-c",
     "copy",
-    `"${saveLocation}\\${vodTitle}.mp4"`,
+    `"${saveLocation}/${vodTitle}.mp4"`,
   ];
 
   fireEvent("merging-parts-starts");
@@ -62,36 +62,29 @@ export async function mergeParts(saveLocation: string, vodTitle: string) {
 }
 
 async function merging(options: string[]) {
+  const prefix = process.platform === "darwin" ? "./" : "";
+
   const mergingProcess = child_process.exec(
-    "ffmpeg " + options.reduce((prev, current) => prev + " " + current),
+    `${prefix}ffmpeg ` + options.reduce((prev, current) => prev + " " + current),
     {
       cwd: PROCESSING_FOLDER,
     }
   );
 
-  try {
-    await new Promise<void>((resolve, reject) => {
-      mergingProcess;
-
-      ipcMain.once("cancel-button-clicked", () => {
-        killer(mergingProcess.pid, "SIGKILL");
-        reject(Error("cancel"));
-      });
-
-      mergingProcess.on("message", (message) => {
-        console.log(message);
-      });
-
-      mergingProcess.on("exit", (code) => {
-        if (mergingProcess.killed) {
-          console.log("Merging process killed");
-          reject(Error("cancel"));
-        }
-        console.log(`Merge process exited with code ${code}`);
-        resolve();
-      });
+  await new Promise<void>((resolve, reject) => {
+    mergingProcess;
+    console.log(mergingProcess.spawnargs);
+    ipcMain.once("cancel-button-clicked", () => {
+      killer(mergingProcess.pid, "SIGKILL");
     });
-  } catch (err) {
-    console.log(err);
-  }
+
+    mergingProcess.on("exit", (code) => {
+      if(code === null){
+        console.log("Merge process killed");
+        reject(Error("cancel"));
+      }
+      console.log(`Merge process exited with code ${code}`);
+      if (code === 0) resolve();
+    });
+  });
 }
